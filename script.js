e42fea286c4cfa54418d2ce0bccf396dbb1dd10c
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-21 11:55:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// script.js
+
+function removeColor() {
+  // Get the reference to the dropdown list
+  var dropdown = document.getElementById("colorSelect");
+
+  // Get the selected index
+  var selectedIndex = dropdown.selectedIndex;
+
+  // Remove the selected option from the dropdown
+  if (selectedIndex !== -1) {
+    dropdown.remove(selectedIndex);
+  }
+}
+
